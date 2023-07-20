58a381325ea7db65987ab45a68ca0d042352e47e
Niharika <saininiharika123@gmail.com>
2023-07-20 13:14:28 +0530

    new look

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,12 +1,23 @@
-import React from "react";
-
+import React, { useState } from "react";
+import DeleteIcon from '@mui/icons-material/Delete';
 const Items=(props)=>{
+    const [line,setLine]=useState(false);
+    const cutIt=()=>{
+        setLine(true)
+    };
+
     return(
         <><div className="list">
-        <li><i className="fa-solid fa-x" onClick={()=>{
+        
+           <span className="delete" onClick={cutIt}> <DeleteIcon 
+            // onClick={()=>{
             // returning the index of current item that has to be deleted
-            props.onSelect(props.idx)
-        }}></i>{props.text}</li>
+            // props.onSelect(props.idx)
+        // }}
+        />
+        </span>
+        {/* teranry operator used for inline style */}
+        <div style={{textDecoration: line ? "line-through":"none"}}>{props.text}</div>
         </div>
         </>
     )
